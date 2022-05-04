d7a12578173d052337aae21a0e6f4046cc94dfae
Gabriel Nobre <wardesiger@gmail.com>
2022-05-04 16:28:42 -0300

    fdodas

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function mensagem(){
+
+    var loginRecebido = document.getElementById('login')
+    var senhaRecebida = document.getElementById('senha')
+
+    if(loginRecebido === 300 && senhaRecebida === 666){
+        alert('Parabéns! Você receberá um prêmio de V. Aguarde atualizaçHAHAHAAHAHAHAHAAH')
+    }
+}
